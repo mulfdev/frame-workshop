1ddc21cc033c58e52e151912d60a975e1e7590cf
mulf <mhmulford0@gmail.com>
2024-02-07 20:09:30 -0600

    add second frame

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,30 @@ app.get("/", (c) =>
         <meta property="fc:frame:button:2" content="Purple" />
         <meta property="fc:frame:button:3" content="Red" />
         <meta property="fc:frame:button:4" content="Blue" />
+
+        <meta property="fc:frame:button:1:action content="post"/>
+        <meta property="fc:frame:button:1:target" content="https://frame-workshop-production-55b0.up.railway.app/button1" />
       </head>
     </html>
 `)
 );
+
+app.post("/button1", (c) => {
+  // https://i.imgur.com/FDD8qwD.jpeg
+  c.text(`
+     <!DOCTYPE html>
+    <html>
+      <head>
+         <meta property="og:title" content="Our First frame" />
+        <meta property="og:image" content="https://i.imgur.com/FDD8qwD.jpeg" />
+				<meta property="fc:frame" content="vNext" />
+				<meta property="fc:frame:image" content="https://i.imgur.com/FDD8qwD.jpeg" />
+     </head>
+    </html>
+
+  `)
+})
+
 console.log("sever is running");
 serve({
   fetch: app.fetch,
